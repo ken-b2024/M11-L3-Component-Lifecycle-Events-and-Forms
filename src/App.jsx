c658bdbc5e115fa79db0b0c962135f10f49e6823
ken-b2024 <kenneth.brown.2404@gmail.com>
2025-04-16 06:50:21 -0400

    M11-L3 Assignment update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import CharacterList from './components/CharacterList';
-import CharacterDetail from './components/CharacterDetail';
 import React, { Component } from 'react';
 import './App.css'
 
@@ -18,7 +17,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app-container">
+      <div>
         <CharacterList
           hoveredCharacter={this.state.hoveredCharacter}
           onHover={this.handleHover}
